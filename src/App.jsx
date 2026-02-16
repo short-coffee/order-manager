30fa4c30d799fe3fabf02626cd48cfc9d6a121b2
nt0v <panag.ntov@gmail.com>
2026-02-16 13:07:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Cafeteria Order Manager</h1>
+            <p>System initialized.</p>
+        </div>
+    )
+}
+
+export default App
